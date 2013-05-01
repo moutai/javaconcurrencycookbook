14e806aae7bea27dd2071a971be0be6b7d7f360e
Moussa Taifi <tub51722@CIS-WCC20918.tu.temple.edu>
2013-05-01 14:53:43 -0400

    added the first example of executor with the ThreadpoolExecutor the newCachedThreadPool

---
+++ src/ch4/ex1/Main.java
@@ -0,0 +1,13 @@
+package ch4.ex1;
+
+public class Main {
+
+	public static void main(String[] args){
+		Server server = new Server();
+		for (int i=0 ; i<100;i++){
+			Task task = new Task("Task "+ i );
+			server.executeTask(task);
+		}
+		server.endServer();
+	}
+}
