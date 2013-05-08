34b7645c612174ca496f60c9d0920d3d3a76118d
moussa taifi <moutai10@gmail.com>
2013-05-08 14:29:41 -0400

    added the separation of task run and process of result

---
+++ src/ch4/ex10/ReportRequest.java
@@ -0,0 +1,20 @@
+package ch4.ex10;
+
+import java.util.concurrent.CompletionService;
+
+public class ReportRequest implements Runnable {
+
+	private String name;
+	private CompletionService<String> service;
+	public ReportRequest(String name, CompletionService<String> service){
+	    this.name=name;
+	    this.service=service;
+	  }
+	 @Override
+	  public void run() {
+	    
+	      ReportGenerator reportGenerator=new ReportGenerator(name, "Report");
+	      service.submit(reportGenerator);
+	    
+	  }
+}
