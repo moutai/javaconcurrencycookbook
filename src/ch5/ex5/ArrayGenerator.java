88147bffeba1d7e58f00f834f08633b9a5388604
moussa taifi <moutai10@gmail.com>
2013-05-10 10:53:16 -0400

    added the cancellation of tasks added the async tasks added exception throwing in the pool

---
+++ src/ch5/ex5/ArrayGenerator.java
@@ -0,0 +1,15 @@
+package ch5.ex5;
+
+import java.util.Random;
+
+public class ArrayGenerator {
+	
+		  public int[] generateArray(int size) {
+		    int array[]=new int[size];
+		    Random random=new Random();
+		    for (int i=0; i<size; i++){
+		      array[i]=random.nextInt(10);
+		    }
+		    return array;
+		  }
+}
